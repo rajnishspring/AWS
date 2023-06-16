ba6bc7647d1884fde608bae789e8fd8ae52ca203
RajnishKumar <rajnishkr114@gmail.com>
2023-06-16 17:45:40 +0530

    testBranch

---
+++ B.java
@@ -0,0 +1,5 @@
+class B{
+public static void main(String [] args){
+system.out.println("This is class B");
+}
+}
---
+++ c.java
@@ -0,0 +1,5 @@
+class C{
+public static void main(String [] args){
+system.out.println("This is class C");
+}
+}
