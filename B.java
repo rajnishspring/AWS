d1b1f371ee407fd39c3913d280c7b4908f8e1822
RajnishKumar <rajnishkr114@gmail.com>
2023-06-16 23:12:22 +0530

    a

--- B.java
+++
@@ -1,5 +0,0 @@
-class B{
-public static void main(String [] args){
-system.out.println("This is class B");
-}
-}
--- c.java
+++
@@ -1,5 +0,0 @@
-class C{
-public static void main(String [] args){
-system.out.println("This is class C");
-}
-}
